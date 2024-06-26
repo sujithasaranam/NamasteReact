a1f3da774a3a5c415d1f31b73d28a2e8d19125e6
sujithasaranam <sujithasaranam@gmail.com>
2024-06-26 08:49:47 +0530

    namaste-react-ep1: seperate js into new file and add css to it

---
+++ App.js
@@ -0,0 +1,3 @@
+const h1 = React.createElement("h1", { id: "heading" }, "Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById("root")); // to create root we use reactdom
+root.render(h1);
