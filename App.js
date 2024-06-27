1a9d0e04bff6c7c6ca3f0c4837d4413f4a738f1b
sujithasaranam <sujithasaranam@gmail.com>
2024-06-27 07:56:03 +0530

    namaste-react-ep1: how to create mutiple parents and its siblings using react.createElement

--- App.js
+++ App.js
@@ -1,3 +1,23 @@
-const h1 = React.createElement("h1", { id: "heading" }, "Hello World from React!");
+/** 
+ * <div id="parent">
+ *     <div id="child">
+ *         <h1>I am a child in h1 tag</h1>
+ *     </div>
+ * </div>
+ * 
+*/
+
+const div = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        [
+            React.createElement("h1", {}, "I am a child in h1 tag"),
+            React.createElement("h2", {}, "I am a child in h2 tag"),
+        ]
+    )
+);
+
+// const h1 = React.createElement("h1", { id: "heading" }, "Hello World from React!");
+// // console.log(h1) // this returns object
 const root = ReactDOM.createRoot(document.getElementById("root")); // to create root we use reactdom
-root.render(h1);
+console.log(div);
+root.render(div);
