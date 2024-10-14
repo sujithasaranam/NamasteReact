41378dc69ad56cc2d2a7fed47c482827dbb52a71
sujithasaranam <sujithasaranam@gmail.com>
2024-10-14 08:24:20 +0530

    namaste-react-ep2: installed react, about parcel uses, browserlist

--- App.js
+++ App.js
@@ -11,6 +11,8 @@
  * </div>
  * 
 */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const div = React.createElement("div", { id: "parent" },
         React.createElement("div", { id: "child" },
