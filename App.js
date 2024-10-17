eb2318c1629757ec91959f8f1546e0e87c50fffb
sujithasaranam <sujithasaranam@gmail.com>
2024-10-17 09:38:47 +0530

    namaste-react-ep2: why jsx, use of jsx

--- App.js
+++ App.js
@@ -14,23 +14,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const div = React.createElement("div", { id: "parent" },
-        React.createElement("div", { id: "child" },
-        [
-            React.createElement("h1", {}, "I am a child in h1 tag"),
-            React.createElement("h2", {}, "I am a child in h2 tag"),
-        ], 
-        React.createElement("div", { id: "child2" },
-            [
-                React.createElement("h1", {}, "I am a child in h1 tag"),
-                React.createElement("h2", {}, "I am a child in h2 tag"),
-            ], 
-        )
-    )
-);
+// react element => object => reactDom => HTML element
+const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
+// using jsx
+const jsxheading = <h1 id="heading">Namaste React using JSX;</h1>
+// Now what heading has is exactly same as jsx heading
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxheading);
+
+
+
+// const div = React.createElement("div", { id: "parent" },
+//         React.createElement("div", { id: "child" },
+//         [
+//             React.createElement("h1", {}, "I am a child in h1 tag"),
+//             React.createElement("h2", {}, "I am a child in h2 tag"),
+//         ], 
+//         React.createElement("div", { id: "child2" },
+//             [
+//                 React.createElement("h1", {}, "I am a child in h1 tag"),
+//                 React.createElement("h2", {}, "I am a child in h2 tag"),
+//             ], 
+//         )
+//     )
+// );
 
 // const h1 = React.createElement("h1", { id: "heading" }, "Hello World from React!");
 // // console.log(h1) // this returns object
-const root = ReactDOM.createRoot(document.getElementById("root")); // to create root we use reactdom
-console.log(div);
-root.render(div);
+// const root = ReactDOM.createRoot(document.getElementById("root")); // to create root we use reactdom
+// console.log(div);
+// root.render(div);
