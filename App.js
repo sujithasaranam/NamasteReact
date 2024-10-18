bbe04b48668deb55461a0a9fda0ff523833e2d5d
sujithasaranam <sujithasaranam@gmail.com>
2024-10-18 09:14:53 +0530

    namaste-react-ep2: what is a compoenent in react and how many types we can write them

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-/** 
+/**
  * <div id="parent">
  *     <div id="child">
  *         <h1>I am a child in h1 tag</h1>
@@ -9,38 +9,47 @@
  *         <h2>I am a child in h1 tag</h2>
  *     </div>
  * </div>
- * 
-*/
+ *
+ */
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 // react element => object => reactDom => HTML element
-const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 // using jsx
-const jsxheading = <h1 id="heading">Namaste React using JSX;</h1>
+// const jsxheading = <h1 id="heading">Namaste React using JSX;</h1>
+const jsxheading = <h1 className="heading">Namaste React using JSX</h1>;
 // Now what heading has is exactly same as jsx heading
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxheading);
 
+const HeadingComponent = () => {
+    return <h1 id="heading"> Namaste react in Functional component with return</h1>;
+};
 
+const HeadingComponent2 = () => (
+    <div id="container">
+        <h1 id="heading"> Namaste react in Functional component without return</h1>
+    </div>
+);
 
+root.render(<HeadingComponent2/>);
 // const div = React.createElement("div", { id: "parent" },
 //         React.createElement("div", { id: "child" },
 //         [
 //             React.createElement("h1", {}, "I am a child in h1 tag"),
 //             React.createElement("h2", {}, "I am a child in h2 tag"),
-//         ], 
+//         ],
 //         React.createElement("div", { id: "child2" },
 //             [
 //                 React.createElement("h1", {}, "I am a child in h1 tag"),
 //                 React.createElement("h2", {}, "I am a child in h2 tag"),
-//             ], 
+//             ],
 //         )
 //     )
 // );
 
 // const h1 = React.createElement("h1", { id: "heading" }, "Hello World from React!");
-// // console.log(h1) // this returns object
+// console.log(h1) // this returns object
 // const root = ReactDOM.createRoot(document.getElementById("root")); // to create root we use reactdom
 // console.log(div);
 // root.render(div);
