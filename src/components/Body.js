60d3a6de9e5870cc93934ad43111fb2ad3bc2215
sujithasaranam <sujithasaranam@gmail.com>
2025-06-23 09:51:59 +0530

    namaste-react-ep6: updated Data with the LiveAPI instead of using the rawData

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
-import resCardData from "../utils/mockData";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useState, useEffect } from "react";
 
 // not sending keys is not acceptable <<< using index <<<< using unique ids as keys
 const Body = () => {
- let [listresCardData, setfilterResCardData] = useState(resCardData);
-  return (
+ let [listresCardData, setListOfRestaurants] = useState([]);
+
+ useEffect(()=>{
+  //console.log("callback function called"); //called after body component was rendered.
+  fetchData()
+ }, []);
+
+const fetchData = async() => {
+  const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8270232&lng=80.240321&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  const json = await data.json();
+  console.log(json)
+  console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+  setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+};
+ //console.log("body rendered"); //THis will be called first as useEffect call back function will be called after body was rendered.
+ //rendering a component on condition is called conditional rendering. 
+ return listresCardData.length == 0 ?  (<Shimmer/>) :
+    (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" onClick={()=>{const filterResData = resCardData.filter((restaurant) => restaurant.info.avgRating > 4.5);
-        setfilterResCardData(filterResData)
+        <button className="filter-btn" onClick={()=>{const filterResData = listresCardData.filter((listresCardData) =>  listresCardData.info.avgRating> 4.5);
+        setListOfRestaurants(filterResData)
     }}>Top Rated Restaurants</button>
       </div>
       <div className="res-container">
